4330d8c57f7c7ac149e5dd79045968056b7aa344
Chen Jiahao <cjh952@gmail.com>
2024-04-06 00:31:47 -0300

    fixing issues
    -fixing switch case
    -add typedef enum of state
    -add comments
    -changing ldr value

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,12 @@
 #define DIA true
 #define NOITE false
 
-// Definição dos Leds para os carros
+// Definição dos Leds para os VEICULOS
 #define C_VERMELHO 2
 #define C_AMARELO 4
 #define C_VERDE 8
+
+#define LO_NIBBLE(b) ((b) & 0x0F)
 
 // Definição dos Leds para os pedestres
 #define P_VERMELHO 16
@@ -20,19 +22,31 @@
 #define BOTAO 6
 
 // Definição dos displays de 7 segmentos
-#define COM_CARROS 5
+#define COM_VEICULOS 5
 #define COM_PEDESTRES 4
 
+typedef enum state_tag
+{
+  ABERTO_PARA_VEICULOS,
+  ABERTO_PARA_PEDESTRES,
+  SEMAFORO_AMARELO,
+  ATUALIZA_DISPLAY_VEICULOS,
+  ATUALIZA_DISPLAY_PEDESTRES,
+
+} state_type;
+
 // Variaveis Globais
-int cont = 0;
+volatile unsigned int cont = 0;
 int cont_ldr = 0;
 int state = 0;
-int cont_disp_c = 0;
+int cont_disp_v = 0;
 int cont_disp_p = 0;
 int ldr = 0;
 int ldr_state = 0;
 bool periodo = true;
-
+int p_disp;
+int v_disp;
+bool blink = false;
 // função timer
 
 void configuracao_Timer0()
@@ -44,6 +58,9 @@ void configuracao_Timer0()
   // Prescaler = escolha um valor entre 1, 8, 64, 256 e 1024
   // Faixa = número de passos em OCR0A (contagem será de 0 até Faixa - 1)
   // Intervalo entre interrupcoes: (Prescaler/Frequência)*Faixa
+
+  TCCR0A = 0;
+  TCCR0B = 0;
 
   // OCR0A – Output Compare Register A
   OCR0A = 124;
@@ -72,7 +89,7 @@ ISR(TIMER0_COMPA_vect)
   // Insira aqui o código da rotina de serviço de interrupção disparada pelo temporizador
   cont++;
   cont_ldr++;
-  cont_disp_c--;
+  cont_disp_v--;
   if (cont_disp_p >= 0)
   {
     cont_disp_p--;
@@ -94,8 +111,8 @@ void setup()
   pinMode(LDR, INPUT);
 
   // Configurando port D como controle do catodo comum do disp de 7 seg
-  DDRD = DDRD | 0b00110000; // define os pinos 4 e 5 do Arduino como saídas, outros pinos como entrada
-  PORTD = 0b0110000;        // Determina estado inicial dos pinos (porta D) com nível lógico baixo
+  DDRD |= 0b00110000;  // define os pinos 4 e 5 do Arduino como saídas, outros pinos como entrada
+  PORTD |= 0b00110000; // Determina estado inicial dos pinos (porta D) com nível lógico baixo
 
   // Configurando port B como saída do display de 7 seg
   DDRB = 0b00001111; // define os pinos 8 a 12 do Arduino como saídas, outros pinos como entrada
@@ -110,19 +127,26 @@ void setup()
 
 void loop()
 {
-
-  ldr = analogRead(LDR);
-
+  // Rotina de medição do LDR e definição do estado DIA e NOITE
+  // ldr: armazena o a leitura analógica do pino do LDR
+  // ldr_state: armazena o estado de quantas medições do ldr foram feitas
+  //            o estado será contado como incremento ou decremento
+  //            será inicializada em 0, enquanto for 0 será considerado NOITE
+  //            quando ocorrerem 3 incrementos(detecção de luz) seguidos será definido como DIA
+  //            da mesma forma, quando ocorrerem 3 decrementos seguidos até 0, será definido como noite
   if (cont_ldr > 126)
   {
 
-    if (ldr < 100 && ldr_state < 3)
+    ldr = analogRead(LDR);
+
+    // Caso a leitura do ldr for menor que 100
+    //
+    if (ldr < 650 && ldr_state < 3)
     {
 
       ldr_state++;
       if (ldr_state >= 3 && periodo != DIA)
       {
-
         periodo = DIA;
         PORTC = (C_VERDE | P_VERMELHO);
         ldr_state = 0;
@@ -130,7 +154,7 @@ void loop()
         state = 0;
       }
     }
-    else if (ldr >= 100 && ldr_state >= 0)
+    else if (ldr >= 650 && ldr_state >= 0 && state == 0)
     {
 
       ldr_state--;
@@ -139,7 +163,7 @@ void loop()
 
         periodo = NOITE;
         PORTC = (C_AMARELO | P_VERMELHO);
-        cont_disp_p = 126;
+        cont = 0;
         ldr_state = 0;
       }
     }
@@ -152,80 +176,94 @@ void loop()
     switch (state)
     {
 
-    case 0:
+    case ABERTO_PARA_VEICULOS:
       if (digitalRead(BOTAO))
       {
-        state = 1;
+        state = SEMAFORO_AMARELO;
         cont = 0;
       }
       break;
 
-    case 1:
+    case SEMAFORO_AMARELO:
 
       if (cont >= 13)
       {
-        state = 2;
+        state = ABERTO_PARA_PEDESTRES;
         PORTC = (C_AMARELO | P_VERMELHO);
         cont = 0;
       }
       break;
 
-    case 2:
+    case ABERTO_PARA_PEDESTRES:
 
       if (cont >= 500)
       {
-        state = 3;
-        // Inicio da contagem do displar
-        // Carros em nove
-        // Pedestre em cinco
+        // Inicio da contagem do display
+        // VEICULOS em nove segundos (1200*8 = 9600ms)
+        // Pedestre em cinco segundos (700*8 = 5600ms)
+        state = ATUALIZA_DISPLAY_VEICULOS;
         PORTC = (C_VERMELHO | P_VERDE);
-        cont_disp_c = 1200;
+        cont_disp_v = 1200;
         cont_disp_p = 700;
         cont = 0;
       }
       break;
 
-    case 3:
-      if (cont >= 1125)
+    case ATUALIZA_DISPLAY_VEICULOS:
+      PORTB = (8*cont_disp_v)/1000;           // 8ms * 1150 = 9.60 s
+      PORTD &= ~(1 << COM_VEICULOS); // Ativa display VEICULOS
+
+      if (cont >= 1){
+        PORTD |= (1 << COM_VEICULOS); // Trava o dígito no display VEICULOS
+        state = ATUALIZA_DISPLAY_PEDESTRES;
+        cont = 0;
+      }
+
+      if (cont_disp_v <= 0)
       {
-        // Contagem do pedestre
+        PORTD |= (1 << COM_VEICULOS); // Trava o dígito no display VEICULOS
         PORTC = (C_VERDE | P_VERMELHO);
-        state = 0;
+        state = ABERTO_PARA_VEICULOS;
+        break;
       }
-      // Seta display
-      // Começa o decremento
-      PORTB = int(8 * cont_disp_c / 1000); // 8ms * 1150 = 9.60 s
-      PORTD &= ~(1 << COM_CARROS);                  // Ativa display carros
-      PORTD |= (1 << COM_CARROS);                   // Trava o dígito no display carros
 
-      PORTB = int(8 * cont_disp_p / 1000); // 8ms * 650 = 5.60 s
+      break;
 
-      if (cont_disp_p >= 63)
-      { // Verifica se a contage esta em valor limiar de 0.5s
-
+    case ATUALIZA_DISPLAY_PEDESTRES:
+      PORTB = (8*cont_disp_p)/1000; // 8ms * 650 = 5.60 s
+      if (cont_disp_p >=63){
         PORTD &= ~(1 << COM_PEDESTRES); // Ativa display pedestre
-        PORTD |= (1 << COM_PEDESTRES);  // Trava o dígito no display pedestre
+      } else if (cont_disp_p <= 0){
+        cont_disp_p += 126;
       }
-      else if (cont_disp_p <= 0)
-      {                     // Quando a contagem do pedestre zerar
-        cont_disp_p += 126; // Incrementa mais 1s para reiniciar o ciclo de blink de 0.5s ligado
-                            // e 0.5s desligado
+
+      if (cont >= 1){
+        PORTD |= (1 << COM_PEDESTRES); // Trava o dígito no display pedestre
+
+        state = ATUALIZA_DISPLAY_VEICULOS;
+        cont = 0;
       }
+
       break;
     }
   }
   else if (periodo == NOITE)
   {
 
-    if (cont_disp_p >= 63)
+    if (cont >= 63 && !blink)
     { // Verifica se a contage esta em valor limiar de 0.5s
       PORTC &= ~(C_AMARELO);
+      Serial.println("apaga amrelo");
+      cont = 0;
+      blink = blink;
     }
-    else if (cont_disp_p <= 0)
+    else if (cont >= 63 && blink)
     { // Quando a contagem do pedestre zerar
       PORTC |= (C_AMARELO);
-      cont_disp_p += 126; // Incrementa mais 1s para reiniciar o ciclo de blink de 0.5s ligado
-                          // e 0.5s desligado
+      Serial.println("acende amarelo");
+      cont = 0;
+      blink = !blink; // Incrementa mais 1s para reiniciar o ciclo de blink de 0.5s ligado
+                 // e 0.5s desligado
     }
   }
 }
